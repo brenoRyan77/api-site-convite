4273afab8d395567853820e07e920c9991d63749
breno.lima <brenoryan21@hotmail.com>
2024-08-09 21:28:19 -0300

    feat: Iniciando Projeto

---
+++ src/main/java/com/br/brenoryan77/api_site_convite/ApiSiteConviteApplication.java
@@ -0,0 +1,13 @@
+package com.br.brenoryan77.api_site_convite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSiteConviteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSiteConviteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/brenoryan77/api_site_convite/ApiSiteConviteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.brenoryan77.api_site_convite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSiteConviteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
